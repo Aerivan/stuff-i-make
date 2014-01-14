7ea60fcb8e4d23983e8d6d3001579fdb2ab19304
Daniel Ström <D@nielstrom.se>
2014-01-14 11:15:58 +0100

    Improve login/logout behavior by showing the correct action and by not needing someone to enter the password if already logged in.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,14 +1,17 @@
 class SessionController < ApplicationController
 	def login
+    if session[:admin?] == :yes
+      redirect_to root_path, notice: 'Already logged in.'
+    end
 	end
 
   def create
-  	if params[:password] == ENV['admin_password']
-  		session[:admin?]  = :yes
-  		redirect_to root_path, notice: 'Successfully logged in.'
-  	else
-  		redirect_to :back, alert: 'Check your caps-lock key and try again.'
-  	end
+    if params[:password] == ENV['admin_password']
+      session[:admin?]  = :yes
+      redirect_to root_path, notice: 'Successfully logged in.'
+    else
+      redirect_to :back, alert: 'Check your caps-lock key and try again.'
+    end
   end
 
   def destroy
